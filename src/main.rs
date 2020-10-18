2e8087329c18004f9473b63e23c0cb472471401d
Johnny DuBois <jddubois@umich.edu>
2020-10-18 17:07:58 -0400

    made it work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use serenity::Client;
+use serenity::model::id::*;
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn cache_ready(&self, _context: Context, guilds: Vec<GuildId>) {
+        let num_guilds = guilds.len();
+        println!("{:#?}", guilds);
+        println!("Guilds in the Cache: {}", num_guilds);
+    }
+    fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!ping" {
+            let _ = msg.channel_id.say(&context, "Pong!");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let token = "NzY3NDUwNDY2NzE5MTA1MDU0.X4yF8Q.WwssW-Fs1zF0icwXvIDaywWh4jk";
+    let mut client = Client::new(&token, Handler).unwrap();
+    if let Err(err) = client.start() {
+        println!("Failed to start client: {:#?}", err);
+    }
 }
